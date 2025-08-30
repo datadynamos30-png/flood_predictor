165bd775ad31a87f1b52d9415e128cf22491c854
datadynamos30-png <datadynamos30@gmail.com>
2025-08-31 01:37:03 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.set_page_config(page_title="Flood Predictor 🌊", page_icon="🌊", layout="wide")
+
+st.title("Flood Predictor 🌊")
+st.success("✅ Your Streamlit app is running on Render!")
